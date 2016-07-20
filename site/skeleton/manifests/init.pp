fb3911ce2047fc465649d23d92b07df390af2184
Cardenas <micardenas@corelogic.com>
2016-07-20 12:02:35 -0700

    test push

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -6,8 +6,6 @@ class skeleton {
 
   file {'/etc/skel/.bashrc':
     ensure => 'file',
-    source => 'puppet:///modules/skeleton/bashrc'
-
-
+    source => 'puppet:///modules/skeleton/files/bashrc'
   }
 }
