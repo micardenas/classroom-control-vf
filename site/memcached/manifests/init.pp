6888cfae62702ebb6a638831a37504bacdf365c3
Michael Cardenas <micardenas@corelogic.com>
2016-07-20 14:42:27 -0700

    memcached

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,20 @@
+class memcached {
+
+  package { 'memcached':
+    ensure => 'present',
+
+  }
+
+
+  file {'/etc/sysconfig/memcached':
+    ensure => 'file',
+    source => 'puppet:///modules/memcached/memcached'
+
+  }
+
+  service {'memcached':
+    ensure => 'running',
+    enable => true,
+    subscribe => File["/etc/sysconfig/memcached"]
+  }
+}
