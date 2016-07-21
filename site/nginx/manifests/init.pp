f85d6708caca716908a159b298f787611d3532f0
Michael Cardenas <micardenas@corelogic.com>
2016-07-21 09:16:29 -0700

    nginx hw try 7

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -19,6 +19,13 @@ class nginx {
     mode   => '0644'
   }
 
+  file { '/etc/nginx/conf.d':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644'
+  }
+
 
   file { '/var/www/index.html':
     ensure    => 'file',
@@ -33,14 +40,22 @@ class nginx {
     owner     => 'root',
     group     => 'root',
     source    => 'puppet:///modules/nginx/nginx.conf',
-    content   => "root /var/www/",
-    subscribe => File["/etc/nginx"],
+    require   => Package['nginx']
+    notify    => Service['nginx']
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure    => 'file',
+    owner     => 'root',
+    group     => 'root',
+    source    => 'puppet:///modules/nginx/nginx.conf',
+    require   => Package['nginx'],
+    notify    => Service['nginx']
   }
 
   service { 'nginx':
     ensure    => 'running',
     enable    => true,
-    subscribe => File["/etc/nginx/nginx.conf"],
   }
 }
 
