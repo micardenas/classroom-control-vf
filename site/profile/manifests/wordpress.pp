2e61e530006dc3b61fca07fbb7288bb9398925f0
Michael Cardenas <micardenas@corelogic.com>
2016-07-22 15:57:00 -0700

    roles

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,4 +1,11 @@
 class profile::wordpress {
 
-  class { 'wordpress': }
+  wordpress::instance { '/opt/wordpresslab':
+    wp_owner    => 'root',
+    wp_group    => 'root',
+    db_user     => 'labuser',
+    db_name     => 'wordpressdb',
+    db_password => 'labpass',
+  }
+
 }
