c3a4dc27d93b4c5529fef79093d5ba2a1fb2ef64
Michael Cardenas <micardenas@corelogic.com>
2016-07-22 15:30:53 -0700

    roles

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,12 +1,12 @@
 class profile::apache {
 
   class {'apache':}
-/*
+
   apache::vhost {'micardenas.lab.com':
     port          => '80',
     docroot       => '/var/www/micardenas',
     docroot_owner => 'root',
     docroot_group => 'root',
   }
-*/
+
 }
